e3ba4d77ffae67218dbcc5088acf6924f1362b29
Bharath Reddy <bharathreddyv2003@gmail.com>
2026-04-23 14:35:32 +0530

    Backend deploy link has changed

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:5000'
+const API_BASE_URL = 'https://demo-x1tt.onrender.com'
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
